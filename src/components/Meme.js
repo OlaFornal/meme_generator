a84b390aa2211b8f2adeed4e2808a2c389eb63a7
Ola Fornal <aleksandra.goluch89@gmail.com>
2022-07-01 15:45:59 +0200

    Selection of a random url

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,24 @@
 import React from "react";
+import memesData from "../memesData.js";
+
+let url;
+
+function getMemeImage() {
+  const memesArray = memesData.data.memes;
+  const randomNumber = Math.floor(Math.random() * memesArray.length);
+  url = memesArray[randomNumber].url;
+}
 
 export default function Meme() {
   return (
     <main>
+      <p>{url}</p>
       <form className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
-        <button className="form--button">Get a new meme image 🖼</button>
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
       </form>
     </main>
   );
